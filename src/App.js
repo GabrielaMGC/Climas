8639ec30297177fe3bd88855c9c7d0521c60d5f2
leticia-f <leticia.fonseca.1106@gmail.com>
2022-03-24 15:21:42 -0300

    Subindo a base do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+const api ={
+  key: "243b6e393ada730d0c460fd5ac464499",
+  base: "https://api.openweathermap.org/data/2.5/",
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <main>
+       <div className="search-box">
+          <input
+             type="text"
+             className="search-bar"
+             placeholder="Digite a cidade..."
+          />
+       </div>
+     </main>
     </div>
   );
 }
