18fda90ade6b4437bc85360e81de964d81985ada
leticia-f <leticia.fonseca.1106@gmail.com>
2022-04-07 15:35:44 -0300

    conexão com a api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+const api = {
+  key: "dbef3485a8fa1a134e9419c78c995b66",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt =>{
+    if (evt.key === "Enter"){
+        fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result =>{
+          setWeather(result);
+          setQuery('');
+        });
+      }
+  }
+
+  const dateBuilder = (d) =>{
+    let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App calor">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Digite a cidade..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}          
+          />
+          </div>
+          <div className='location-box'>
+            <div className='location'>Taubaté, Brasil</div>
+            <div className='date'>{dateBuilder(new Date())}</div>
+          </div>
+          <div className='weather-box'>
+             <div className='temp'>
+                15ºC
+             </div>
+             <div className='weather'>
+                Sunny
+            </div>
+           </div>
+      </main>
     </div>
   );
 }
